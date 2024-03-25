42365f43feb4b0a6ebc8eb5dc3b9968712ef21cb
Stephen Sellers <stephensellers034@yahoo.com>
2024-03-25 10:13:47 -0400

    MethodsExercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -13,7 +13,8 @@
             Console.WriteLine("Who is your favorite band?");
             string band = Console.ReadLine();
             Console.WriteLine($"Hello, {name}. My name is Stephen. What is your favorite color? {color}. I love the color Blue. My favorite animal is a dog. Whats Your favorite animal? {animal}. I like the Rolling Stones. Who is your favorite band? {band}.");
-            Add(10,5);
+            int y = Add(10,5,12,14,34);
+            Console.WriteLine(y);
             Subtract(10, 5);
             Multiply(10, 5);
             Divide(10, 5);
@@ -47,11 +48,14 @@
             Console.WriteLine($"My favorite Band is {band}");
         }
 
-        public static int Add(int numb1, int numb2)
+        public static int Add(params int[] ListNumbers)
         {
-            int sum = numb1 + numb2;
-            Console.WriteLine(sum);
-            return sum;
+            int total = 0;
+            foreach (int i in ListNumbers)
+            {
+                total += i;
+            }
+            return total;
         }
 
         public static int Subtract(int numb1, int numb2)
